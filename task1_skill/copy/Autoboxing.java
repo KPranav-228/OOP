a8fa98523b3fc1f0b22714c85407064156c4e343
KPranav-228 <2420030228@klh.edu.in>
2025-07-24 11:41:32 +0530

    Skill

---
+++ task1_skill/copy/Autoboxing.java
@@ -0,0 +1,13 @@
+package task1_skill.copy;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+	float c=9.2f;
+	Float j=Float.valueOf(c);
+	Float i=c;
+	System.out.println(c+" "+i+" "+j);
+
+	}
+
+}
---
+++ task1_skill/copy/Unboxing.java
@@ -0,0 +1,11 @@
+package task1_skill.copy;
+
+public class Unboxing {
+	public static void main(String[] args) {
+		Float k= new Float(5.2f);
+		float j=k;
+		float i=k.floatValue();
+		System.out.println(k+" "+j+" "+i);
+
+	}
+}
